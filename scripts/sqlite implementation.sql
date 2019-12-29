7d8561492c98e9ab08096c2b303898d8adc06f34
Omar <omarweeb@gmail.com>
2019-12-29 17:39:16 -0600

    Update sqlite implementation.sql

--- scripts/sqlite implementation.sql
+++ scripts/sqlite implementation.sql
@@ -1,42 +1,69 @@
 /* finding best start point
-rId: 8705
-bestStartPoint: ?--95
-endPoint int = 42
-latitude: 19.422123631224547
-longitude: -102.07343347370625
-distance: 0.01
+ruta 15
+rId: 8702
+walkDistLimit: 0.0055376097498
+origen: 19.40166, -102.05550
+desstino: 19.43052, -102.05597
+
+startPoint: 149
+endPoint: 433
+
+WD_WEIGHT = 0.3
+RD_WEIGHT = 0.7
+
+bestStartPoint:
+bestEndPoint:
+
 
 */
-drop table if exists bestpoints;
-create temp table bestpoints(
-	pointId int,
-	routeId int,
-	lat real,
-	lng real,
-	number int,
-	distanceToNextPoint int,
-	wd real,
-	rd real,
-	betterness real
-);
 
-insert into bestpoints
-select p1.pointId, p1.routeId, p1.lat, p1.lng,p1.number
-	,p1.distanceToNextPoint,
-	((19.422123631224547 - p1.lat)*(19.422123631224547 - p1.lat) + (-102.07343347370625 - p1.lng)*(-102.07343347370625 - p1.lng))  [wd],
-	(SELECT SUM(p2.distanceToNextPoint) 
-		FROM Points p2
-		WHERE p2.routeId = 8705
-		AND ( ((p1.number > 42) AND (p2.number >= p1.number OR p2.number <= 42)) 
-		   OR ((p1.number < 42) AND (p2.number BETWEEN p1.number AND 42)))
-	) [rd],
-	null
-from Points p1
-where p1.routeId = 8705
-AND p1.lat between (19.422123631224547-0.01) and (19.422123631224547+0.01)
-AND p1.lng between (-102.07343347370625-0.01) and (-102.07343347370625+0.01);
+-- walkDistLimit esta en metros * metro_ang
+-- distanceToNextPoint esta en metros?
 
-update bestpoints
-set betterness = ((((select min(wd) from bestpoints)/wd)*0.3) + ((((select min(rd) from bestpoints)/rd)*0.7)));
+-- encontrando bestStartPoint con endPoint y origen
+delete from bestPoints;
 
-select *, ((select min(wd) from bestpoints)/wd) [minWd/wd], ((select min(rd) from bestpoints)/rd) [minRd/rd] from bestpoints order by betterness desc 
+INSERT INTO bestPoints  
+SELECT p1.pointId, p1.routeId, p1.lat, p1.lng, p1.number, p1.distanceToNextPoint,  
+	((19.40166 - p1.lat)*(19.40166 - p1.lat) + (-102.05550 - p1.lng)*(-102.05550 - p1.lng))  [wd],  
+	(SELECT SUM(p2.distanceToNextPoint)   
+	FROM Points p2  
+	WHERE p2.routeId = 8702  
+	AND ( ((p1.number > 433) AND (p2.number >= p1.number OR p2.number <= 433))   
+		OR ((p1.number < 433) AND (p2.number BETWEEN p1.number AND 433)))  
+	) * 0.000009229349583 [rd],  
+null  
+FROM Points p1  
+WHERE p1.routeId = 8702  
+AND p1.lat BETWEEN (19.40166 - 0.0055376097498) AND (19.40166 + 0.0055376097498)  
+AND p1.lng BETWEEN (-102.05550 - 0.0055376097498) AND (-102.05550 + 0.0055376097498);
+
+UPDATE bestPoints SET betterness = ((((SELECT MIN(wd) FROM bestPoints)/wd)*0.3) + ((((SELECT MIN(rd) FROM bestPoints)/rd)*0.7)));
+
+SELECT pointId, routeId, lat, lng, number, distanceToNextPoint  FROM bestPoints ORDER BY betterness DESC LIMIT 1;
+-- resultado bestStartPoint: 345
+-- deberia ser 309, 310
+
+
+-- encontrando bestEndPoint con startPoint y destino
+delete from bestPoints;
+
+INSERT INTO bestPoints 
+SELECT p1.pointId, p1.routeId, p1.lat, p1.lng, p1.number, p1.distanceToNextPoint, 
+((19.43052 - p1.lat)*(19.43052 - p1.lat) + (-102.05597 - p1.lng)*(-102.05597 - p1.lng))  [wd], 
+(SELECT SUM(p2.distanceToNextPoint)  
+FROM Points p2 
+WHERE p2.routeId = 8702 
+AND ( ((149 > p1.number) AND (p2.number >= 149 OR p2.number <= p1.number))  
+   OR ((149 < p1.number) AND (p2.number BETWEEN 149 AND p1.number))) 
+) [rd], 
+null 
+FROM Points p1 
+WHERE p1.routeId = 8702 
+AND p1.lat BETWEEN (19.43052 -  0.0055376097498) AND (19.43052 +  0.0055376097498) 
+AND p1.lng BETWEEN (-102.05597 -  0.0055376097498) AND (-102.05597 +  0.0055376097498);
+
+UPDATE bestPoints SET betterness = ((((SELECT MIN(wd) FROM bestPoints)/wd)*0.3) + ((((SELECT MIN(rd) FROM bestPoints)/rd)*0.7)));
+
+SELECT pointId, routeId, lat, lng, number, distanceToNextPoint  FROM bestPoints ORDER BY betterness DESC LIMIT 1;
+-- resultado bestEndPoint: 433
