5c8dad56bef68420db2e16bf2cd957802b51db6f
Omar Rico <omarweeb@gmail.com>
2025-07-01 14:32:36 -0600

    Refactor: Update method to check for internet connection

--- RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/Utils.kt
+++ RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/Utils.kt
@@ -3,6 +3,7 @@ package com.rico.omarw.rutasuruapan
 import android.content.Context
 import android.graphics.Typeface
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.IBinder
 import android.text.SpannableString
 import android.text.Spanned
@@ -10,23 +11,38 @@ import android.text.style.StyleSpan
 import android.view.inputmethod.InputMethodManager
 
 
-object Utils{
-    fun hideKeyboard(context: Context, windowToken: IBinder?){
-        if(windowToken!= null)
-            (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(windowToken,0)
+object Utils {
+    fun hideKeyboard(context: Context, windowToken: IBinder?) {
+        if (windowToken != null)
+            (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+                windowToken,
+                0
+            )
     }
 
     fun formatRouteTitle(name: String, shortName: String): SpannableString {
         val spannableString = SpannableString("$name #$shortName")
-        spannableString.setSpan(StyleSpan(Typeface.BOLD), spannableString.length - shortName.length, spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(
+            StyleSpan(Typeface.BOLD),
+            spannableString.length - shortName.length,
+            spannableString.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
         return spannableString
     }
 
-    //todo: update deprecated method
     fun checkInternetConnection(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = cm.activeNetworkInfo
-        return activeNetwork != null && activeNetwork.isConnected
+        val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+
+        return when {
+            capabilities == null -> false
+            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                -> true
+
+            else -> false
+        }
     }
 }
