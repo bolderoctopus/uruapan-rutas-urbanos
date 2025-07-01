5e79ec90d914fe852022624a22ab10d8bc1fea41
Omar Rico <omarweeb@gmail.com>
2025-07-01 14:46:36 -0600

    Refactor: Remove force unwraps in RouteModel.kt

--- RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/models/RouteModel.kt
+++ RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/models/RouteModel.kt
@@ -4,8 +4,9 @@ import android.util.SparseArray
 import com.google.android.gms.maps.model.*
 import com.rico.omarw.rutasuruapan.database.Point
 import com.rico.omarw.rutasuruapan.database.Route
+import androidx.core.util.size
 
-class RouteModel (val routeDb : Route){
+class RouteModel(val routeDb: Route) {
     var mainSegment: Polyline? = null
     var secondarySegment: Polyline? = null
     var isDrawn: Boolean = false
@@ -19,13 +20,15 @@ class RouteModel (val routeDb : Route){
     var endPoint: Point? = null
     var walkDist: Double? = null
     var totalDist: Double? = null
+
     @Deprecated("No longer needed since a route result now includes a startCap")
     var startMarker: Marker? = null
+
     @Deprecated("No longer needed since a route result now includes an endCap")
     var endMarker: Marker? = null
     var mainSegmentMarkers: List<Marker>? = null
 
-    fun setVisibility(visible: Boolean){
+    fun setVisibility(visible: Boolean) {
         startMarker?.isVisible = visible
         endMarker?.isVisible = visible
         mainSegment?.isVisible = visible
@@ -36,7 +39,7 @@ class RouteModel (val routeDb : Route){
         isDrawn = visible
     }
 
-    fun remove(){
+    fun remove() {
         startMarker?.remove()
         endMarker?.remove()
         mainSegment?.remove()
@@ -44,9 +47,9 @@ class RouteModel (val routeDb : Route){
         polyline?.remove()
         mainSegmentMarkers?.forEach { it.remove() }
 
-        if(directionalMarkers != null) {
-            for (x in 0 until directionalMarkers!!.size()){//todo: force unwrap
-                directionalMarkers!!.valueAt(x).forEach { it.remove() }
+        directionalMarkers?.let { markers ->
+            for (x in 0 until markers.size) {
+                markers.valueAt(x).forEach { it.remove() }
             }
         }
 
@@ -54,47 +57,64 @@ class RouteModel (val routeDb : Route){
     }
 
     override fun equals(other: Any?): Boolean {
-        if(this === other) return true
-        if(other == null || javaClass != other.javaClass) return false
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
         val model = other as RouteModel
 
-        if(id != model.id) return false
+        if (id != model.id) return false
         return name == model.name && color == model.color
     }
 
-    fun getMainSegment(points: List<Point>) = getRouteSegment(startPoint!!.number, endPoint!!.number, points)//todo: force unwrap
-    fun getSecondarySegment(points: List<Point>) = getRouteSegment(endPoint!!.number, startPoint!!.number, points)
+    fun getMainSegment(points: List<Point>) =
+        getRouteSegment(startPoint, endPoint, points)
+
+    fun getSecondarySegment(points: List<Point>) =
+        getRouteSegment(endPoint, startPoint, points)
 
 
-    private fun getRouteSegment(start: Int, end: Int, points: List<Point>): Iterable<LatLng>{
+    private fun getRouteSegment(start: Point?, end: Point?, points: List<Point>): Iterable<LatLng> {
+        if (start == null || end == null) return emptyList()
+
+        val startNumber = start.number
+        val endNumber = end.number
+
         val segment = ArrayList<LatLng>()
-        if(start > end){
-            val part1 = points.filter {it.number >= start}.sortedBy {it.number}
-            val part2 = points.filter {it.number <= end }.sortedBy {it.number}
+
+        if (startNumber > endNumber) {
+            val part1 = points.filter { it.number >= startNumber }.sortedBy { it.number }
+            val part2 = points.filter { it.number <= endNumber }.sortedBy { it.number }
             (part1 + part2).forEach {
                 segment.add(LatLng(it.lat, it.lng))
             }
-        }else{
-            points.filter {it.number in start..end}.sortedBy {it.number}.forEach{
+        } else {
+            points.filter { it.number in startNumber..endNumber }.sortedBy { it.number }.forEach {
                 segment.add(LatLng(it.lat, it.lng))
             }
         }
         return segment
     }
 
-    fun getMainSegmentPoints(points: List<Point>) = getRouteSegmentPoints(startPoint!!.number, endPoint!!.number, points)//todo: force unwrap
-    private fun getRouteSegmentPoints(start: Int, end: Int, points: List<Point>): List<Point>{
-        return if(start > end){
-            val part1 = points.filter {it.number >= start}.sortedBy {it.number}
-            val part2 = points.filter {it.number <= end }.sortedBy {it.number}
+    fun getMainSegmentPoints(points: List<Point>) =
+        getRouteSegmentPoints(startPoint, endPoint, points)
+
+    private fun getRouteSegmentPoints(start: Point?, end: Point?, points: List<Point>): List<Point> {
+        if (start == null || end == null) return emptyList()
+
+        val startNumber = start.number
+        val endNumber = end.number
+
+
+        return if (startNumber > endNumber) {
+            val part1 = points.filter { it.number >= startNumber }.sortedBy { it.number }
+            val part2 = points.filter { it.number <= endNumber }.sortedBy { it.number }
             (part1 + part2)
-        }else{
-            points.filter {it.number in start..end}.sortedBy {it.number}
+        } else {
+            points.filter { it.number in startNumber..endNumber }.sortedBy { it.number }
         }
     }
 
-    companion object{
-        val dashedPatter: List<PatternItem> = ArrayList<PatternItem>().apply{
+    companion object {
+        val dashedPatter: List<PatternItem> = ArrayList<PatternItem>().apply {
             add(Gap(40f))
             add(Dash(20f))
         }
