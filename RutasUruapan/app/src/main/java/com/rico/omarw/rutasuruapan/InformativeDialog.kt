8ee33e7f3b087669f2ef5009ae06eda3946d690b
Omar Rico <omarweeb@gmail.com>
2025-07-01 13:50:03 -0600

    Refactor: Make InformativeDialog.window non-nullable

--- RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/InformativeDialog.kt
+++ RutasUruapan/app/src/main/java/com/rico/omarw/rutasuruapan/InformativeDialog.kt
@@ -3,42 +3,50 @@ package com.rico.omarw.rutasuruapan
 import android.content.Context
 import android.content.DialogInterface.OnDismissListener
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.graphics.drawable.toDrawable
 
-class InformativeDialog{
-    enum class Style{
+class InformativeDialog {
+    enum class Style {
         Left,
         Center
     }
 
-    companion object{
-        fun show(context: Context,verticalOffset: Int, style: Style, message: Int, onDismissListener: OnDismissListener? ){
+    companion object {
+        fun show(
+            context: Context,
+            verticalOffset: Int,
+            style: Style,
+            message: Int,
+            onDismissListener: OnDismissListener?
+        ) {
             val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_help, null)
 
-            when(style){
+            when (style) {
                 Style.Left -> dialogView.setBackgroundResource(R.drawable.dialog_background_left)
                 Style.Center -> dialogView.setBackgroundResource(R.drawable.dialog_background_center)
             }
 
             val dialog = AlertDialog.Builder(context)
-                    .setView(dialogView)
-                    .setOnDismissListener(onDismissListener)
-                    .create()
+                .setView(dialogView)
+                .setOnDismissListener(onDismissListener)
+                .create()
 
-            dialog.window?.decorView?.background = ColorDrawable(Color.TRANSPARENT)
-            dialog.window?.decorView?.setPadding(0,0,0,0)
+            dialog.window?.let {
+                it.decorView.background = Color.TRANSPARENT.toDrawable()
+                it.decorView.setPadding(0, 0, 0, 0)
 
-            dialog.window!!.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)//todo: force unwrap
-            dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
-            dialog.window!!.attributes.apply {
-                gravity = Gravity.BOTTOM
-                y = verticalOffset
+                it.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+                it.requestFeature(Window.FEATURE_NO_TITLE)
+                it.attributes.apply {
+                    gravity = Gravity.BOTTOM
+                    y = verticalOffset
+                }
             }
 
             dialogView.findViewById<TextView>(R.id.textview_message)?.setText(message)
